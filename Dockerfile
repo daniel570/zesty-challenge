8d137966bb6037ece58501aab96ce8753d917fc7
daniel570 <danielharsheffer@gmail.com>
2023-05-12 00:16:41 +0300

    added dockerfile & requirements

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+# Use the official Python image as the base image
+FROM python:3.9-slim
+
+# Set the working directory in the container
+WORKDIR /app
+
+# Copy the requirements.txt file to the container
+COPY requirements.txt .
+
+# Install the dependencies
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copy the entire app directory to the container
+COPY . /app
+
+# Set the environment variables
+ENV PORT=5050
+ENV AWS_ACCESS_KEY_ID=""
+ENV AWS_SECRET_ACCESS_KEY=""
+ENV AWS_DEFAULT_REGION=""
+
+# Expose the port that the app will run on
+EXPOSE $PORT
+
+# Start the Flask app
+CMD ["python", "app/router.py"]
--- app/app.py
+++ app/app.py
@@ -1,29 +1,51 @@
+from flask import Flask, jsonify
 import boto3
+import os
 
-# Configure AWS credentials and region
-aws_access_key_id = 'your_access_key_id'
-aws_secret_access_key = 'your_secret_access_key'
-aws_region = 'your_dynamodb_region'
+aws_access_key_id = os.environ.get("AWS_ACCESS_KEY_ID")
+aws_secret_access_key = os.environ.get("AWS_SECRET_ACCESS_KEY")
+region_name = os.environ.get("AWS_REGION")
+
+app = Flask(__name__)
 
 # Create DynamoDB client
 dynamodb = boto3.client('dynamodb',
+                        endpoint_url='http://localhost:8000',
                         aws_access_key_id=aws_access_key_id,
                         aws_secret_access_key=aws_secret_access_key,
-                        region_name=aws_region)
+                        region_name=region_name)
 
-table_name = 'devops-challenge'
-code_name = 'YOUR_CODENAME'  # Replace with your codename
-
-# Retrieve the item from DynamoDB
-response = dynamodb.get_item(
-    TableName=table_name,
-    Key={
-        'codeName': {'S': code_name}
+@app.route('/health')
+def health():
+    response = {
+        "status": "Healthy!",
+        "container": "daniel570/zesty-challenge:latest"
     }
-)
+    return jsonify(response)
 
-# Extract the secret string from the response
-item = response.get('Item')
-secret_string = item.get('secretCode').get('S') if item else ''
+@app.route('/secret')
+def secret():
+    table_name = 'devops-challenge'
+    code_name = 'theDoctor'
 
-print('Secret String:', secret_string)
+    # Retrieve the item from DynamoDB
+    response = dynamodb.get_item(
+        TableName=table_name,
+        Key={
+            'codeName': {'S': code_name}
+        }
+    )
+
+    # Extract the secret string from the response
+    item = response.get('Item')
+    secret_code = item.get('secretCode').get('S') if item else ''
+
+    result = {
+        'codeName': code_name,
+        'secretCode': secret_code
+    }
+
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5050)
